b9d2527b39501f3d273d7975d4ca8c71f48dda99
Mihnea Giurgea <mihnea.giurgea@ubervu.com>
2013-12-09 13:19:09 +0200

    PlayerBattleground can now serialize & deserialize objects

--- src/main/scala/PlayerBattleground.scala
+++ src/main/scala/PlayerBattleground.scala
@@ -8,6 +8,11 @@ object PlayerBattleground {
     creatures.foldLeft(z)(_ + _)
   }
 
+  def fromString(s: String): PlayerBattleground = {
+    val split = s.split(", ").toList.filter(_.length > 0)
+    PlayerBattleground(split.map(Creature.fromString(_)))
+  }
+
 }
 
 /** A collection for all Creatures under some player's control.
@@ -26,4 +31,5 @@ case class PlayerBattleground private(creatures: List[Creature]) {
   def removeAt(index: Int): PlayerBattleground =
     PlayerBattleground(creatures.take(index) ::: creatures.drop(index + 1))
 
+  override def toString = creatures.mkString(", ")
 }
--- src/test/scala/PlayerBattlegroundSpec.scala
+++ src/test/scala/PlayerBattlegroundSpec.scala
@@ -9,7 +9,7 @@ class PlayerBattlegroundSpec extends FlatSpec {
 
   val c1 = Creature(CreatureCard(1, 1))
   val c2 = Creature(CreatureCard(2, 2))
-  val c3 = Creature(CreatureCard(3, 3))
+  val c3 = Creature(CreatureCard(3, 3), tapped=true)
 
   "PlayerBattleground" should "add creatures in sorted order" in {
     assert(PlayerBattleground(c1, c2, c3) === PlayerBattleground(c3, c2, c1))
@@ -23,4 +23,11 @@ class PlayerBattlegroundSpec extends FlatSpec {
     assert(PlayerBattleground(c1, c2, c3).removeAt(0) === PlayerBattleground(c3, c2))
   }
 
+  it should "serialize and deserialize objects" in {
+    val obj = PlayerBattleground(c1, c2, c3)
+    assert(obj === PlayerBattleground.fromString(obj.toString))
+
+    val emptyObj = PlayerBattleground()
+    assert(emptyObj === PlayerBattleground.fromString(emptyObj.toString))
+  }
 }
